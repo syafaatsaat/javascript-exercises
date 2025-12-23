74dad1a89e70ad85728dcba68d00d6d469572fc3
syafaatsaat <47024629+syafaatsaat@users.noreply.github.com>
2025-12-23 10:38:16 +0800

    Complete Chapter 14, 15, 16 (foundations)

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if (number < 0) {
+        return "OOPS";
+    }
+    
+    let fibArray = [0, 1];
+    if (number < 2) {
+        return fibArray[number];
+    }
 
+    for (let i = 2; i <= number; ++i) {
+        fibArray.push(fibArray[i - 1] + fibArray[i - 2]);
+    }
+
+    return fibArray[number];
 };
 
 // Do not edit below this line
--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const sortedPeople = people.sort((a, b) => {
+        let currentYear = (new Date()).getFullYear();
+        let ageA = 0;
+        if ('yearOfDeath' in a) {
+            ageA = a.yearOfDeath - a.yearOfBirth;
+        }
+        else {
+            ageA = currentYear - a.yearOfBirth;
+        }
 
+        let ageB = 0;
+        if ('yearOfDeath' in b) {
+            ageB = b.yearOfDeath - b.yearOfBirth;
+        }
+        else {
+            ageB = currentYear - b.yearOfBirth;
+        }
+
+        return (ageB < ageA) ? -1 : 1;
+    });
+    return sortedPeople[0];
 };
 
 // Do not edit below this line
