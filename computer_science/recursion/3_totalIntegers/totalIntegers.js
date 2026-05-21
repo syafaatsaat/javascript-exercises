fc53c995a53d6a1e53f269af7563bf66eee97394
Syafaat Saat <47024629+syafaatsaat@users.noreply.github.com>
2026-05-21 15:28:59 +0800

    Finish 3_totalIntegers of computer_science

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,5 +1,21 @@
-const totalIntegers = function() {
+const totalIntegers = function(input) {
+  if (typeof input !== 'object' && !Array.isArray(input)) {
+    return undefined;
+  }
+
+  let inputArray = input;
+  if (typeof input === 'object')
+    inputArray = Object.values(input);
+
+  let total = inputArray
+    .filter(
+      (elem) => 
+        elem !== null && (typeof elem === 'object' || Array.isArray(elem))
+    )
+    .map((elem) => totalIntegers(elem))
+    .reduce((accum, curr) => accum + curr, 0);
   
+  return total + inputArray.filter((elem) => Number.isInteger(elem)).length;
 };
   
 // Do not edit below this line
