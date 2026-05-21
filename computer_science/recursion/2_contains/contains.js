eb40796e46a8dc4032d284547a12892fa04a15ef
Syafaat Saat <47024629+syafaatsaat@users.noreply.github.com>
2026-05-21 13:31:05 +0800

    Finish 2_contains of computer_science

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,5 +1,30 @@
-const contains = function() {
-  
+const contains = function(obj, variable) {
+  // const values = Object.values(obj);
+
+  // if (values.includes(variable))
+  //   return true;
+
+  // const nestedObjects = values.filter((elem) => 
+  //   elem !== null && typeof elem === 'object'
+  // );
+
+  // return nestedObjects.some((nestedObj) => 
+  //   contains(nestedObj, variable)
+  // );
+
+  if (obj === variable || Object.is(obj, variable))
+    return true;
+
+  let outcome = false;
+  if (obj !== null && typeof obj === 'object' && !Array.isArray(obj)) {
+    Object.values(obj).forEach(elem => {
+      outcome = outcome || contains(elem, variable);
+      if (outcome)
+        return true;
+    });
+  }
+
+  return outcome;
 };
   
 // Do not edit below this line
