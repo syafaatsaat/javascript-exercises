29469cd47dc3feb6243f46adfa3bdd190a907726
Syafaat Saat <47024629+syafaatsaat@users.noreply.github.com>
2026-05-21 16:10:08 +0800

    Finish 4_permutations of computer_science

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,5 +1,17 @@
-const permutations = function() {
-  
+const permutations = function(arr) {
+  if (arr.length <= 1)
+    return [arr];
+
+  const perms = [];
+  for (let i = 0; i < arr.length; ++i) {
+    let remainingElems = arr.toSpliced(i, 1);
+
+    permutations(remainingElems).forEach(
+      (subPerms) => perms.push([...[arr[i]], ...subPerms])
+    );
+  }
+
+  return perms;
 };
   
 // Do not edit below this line
