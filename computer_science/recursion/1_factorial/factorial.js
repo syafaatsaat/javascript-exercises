cd5ba02a8531143c8f6ffab407695cc7714ed605
Syafaat Saat <47024629+syafaatsaat@users.noreply.github.com>
2026-05-21 11:35:19 +0800

    Finish 1_factorial of computer_science

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,5 +1,11 @@
-const factorial = function() {
+const factorial = function(n) {
+  if (!Number.isInteger(n) || n < 0)
+    return undefined;
 
+  if (n === 0)
+    return 1;
+
+  return n * factorial(n - 1);
 };
 
 // Do not edit below this line
