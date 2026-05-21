2a7f6c8a9ca5865991914e7bfaaac7ba372e2602
Syafaat Saat <47024629+syafaatsaat@users.noreply.github.com>
2026-05-21 21:16:45 +0800

    Finish 5_pascal of computer_science

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,5 +1,24 @@
-const pascal = function() {
-  
+const pascal = function(n) {
+  if (n === 1)
+    return [1];
+
+  const prevRow = pascal(n - 1);
+  let currRow = [];
+  for (let i = 0; i < n; ++i) {
+    let left = i - 1;
+    let right = i;
+    let val = 0;
+
+    if (left >= 0)
+      val += prevRow[left];
+
+    if (right < prevRow.length)
+      val += prevRow[right];
+
+    currRow.push(val);
+  }
+
+  return currRow;
 };
   
 // Do not edit below this line
